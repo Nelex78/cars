afed2e7818270f8dccb414420e588264ca6cf372
Nenad Dinic <dinicnenad@gmail.com>
2018-03-25 16:38:17 +0200

    commit_2 adding cars

--- js/main.js
+++ js/main.js
@@ -2,7 +2,25 @@ $( document ).ready(function() {
      
     $.getJSON("data.json", function(data) {
         var data = data;
-  
+        var carsInfo = data.cars;
+        var carData = '';
+        var carsLIst = document.getElementById('cars-list');  
+        
+
+
+        //car elements
+        carsInfo.forEach(function(car, index) {
+            carData += 
+                '<div class="cars box-' + index + '"><div class="panel"><div class="front card"><img src="' + car.image + '" alt="' + car.name + '"/> <p class="carname">' + car.name + '</p> </div><div class="back card"><img src="' + car.image + '" alt="' + car.name + '"/><div class="desc"><p><span class="block">Description: </span>' + car.description + '</p> <p><span>Speed: </span>' + car.speed + 'kmh</p></div> </div></div></div>';
+        });
+        carsLIst.innerHTML = carData;
+
+
+
+
+
+
+
     });
   
 }) 
